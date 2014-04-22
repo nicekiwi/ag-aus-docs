36421f8bef8958e7974f430d231c3a8f6056be33
Ezra Sharp <ezra@kiwidev.co.nz>
2014-04-22 15:08:36 +1200

    Simple implementation of deployment and documented.

--- github-deploy.php
+++ github-deploy.php
@@ -1,8 +1,9 @@
 <?php // Pull chnages from git.
 
+// Goto scripts location.
 chdir(__DIR__);
-$cmd = shell_exec('git pull 2>&1');
 
+// Echo the output of the 'git pull' command.
 echo '<pre>';
-passthru($cmd);
+echo shell_exec('git pull 2>&1');
 echo '</pre>';
